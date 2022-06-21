57154bbb7f6a32400744b2e71dec18394ef78bc7
demondandy <okorodandyj@gmail.com>
2022-06-21 14:06:33 -0700

    Updated Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+
+''' Square here '''
+
 class Square:
-    """Represents a square.
+    '''Represents a square.
     Private instance attribute: size.
     Instantiation with optional size.
     Public instance method: def area(self).
-    """
+    '''
 
     def __init__(self, size=0):
-        """Initializes the data."""
+        '''Initializes the data.'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
